8b07a8994c7d6b2ff1f61144239ca492b5aef521
neetcode <neetcodebusiness@gmail.com>
2022-10-02 07:15:05 -0700

    more files

---
+++ loops.py
@@ -0,0 +1,20 @@
+n = 0
+while n < 5:
+    print(n)
+    n += 1
+>>> 0 1 2 3 4
+
+# Looping from i = 0 to i = 4
+for i in range(5):
+    print(i)
+>>> 0 1 2 3 4
+
+# Looping from i = 2 to i = 5
+for i in range(2, 6):
+    print(i)
+>>> 2 3 4 5
+
+# Looping from i = 5 to i = 2
+for i in range(5, 1, -1):
+    print(i)
+>>> 5 4 3 2
